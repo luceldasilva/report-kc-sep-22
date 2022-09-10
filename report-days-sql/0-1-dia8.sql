f79eb423ba65a979d9da82da36e523a7771e0238
Lucel Da Silva <luceldasilva@yahoo.com>
2022-09-09 23:05:45 -0400

    Reporte de los dias 8-9 y armado del index

---
+++ report-days-sql/0-1-dia8.sql
@@ -0,0 +1,136 @@
+-- 8/09/22
+insert into kcsep22 (usuario, deck, skill, wins, ndmax, zerotg, zephra, bryan, xenoblur) 
+values 
+	('0953', 'Tech Genus', 'Control de Vuelo', 62, '6/9/2022', true, false, false, false),
+	('0986', 'Rose Dragon', 'Conjuro de Rosas', 102, '6/9/2022', true, false, false, false),
+	('9310', 'Phantom Knights', 'Estrategia Fantasma', 156, '6/9/2022', true, false, false, false),
+	('6100', 'Rose Dragon', 'Conjuro de Rosas', 45, '6/9/2022', true, false, false, false),
+	('4074', 'Elemental Heros', 'Duelo Favorito', 163, '6/9/2022', true, false, false, false),
+	('9651', 'Fossil', 'Agujero de Gólem', 148, '6/9/2022', true, false, false, false),
+	('3867', 'Infernity', 'Fantasma Hueco', 90, '7/9/2022', true, false, false, false),
+	('4162', 'Meklord', 'Refinamiento Meklord', 92, '7/9/2022', true, false, false, false),
+	('3107', 'Ice Barrier', 'Territorio de Tiburones', 154, '7/9/2022', true, false, false, false),
+	('9361', 'Necroz', 'Maestro de Rituales II', 60, '7/9/2022', true, false, false, false),
+	('5498', 'Malefic', 'Territorio de Malicia', 52, '7/9/2022', true, false, false, false),
+	('9851', 'Galaxy-Eyes', 'Galaxia XYZ', 90, '7/9/2022', true, false, false, false),
+	('4536', 'Heros', 'Robar sentido Viento', 54, '7/9/2022', true, false, false, false),
+	('7061', 'Rose Dragon', 'Conjuro de Rosas', 104, '7/9/2022', true, false, false, false),
+	('1892', 'Phantom Knights', 'Estrategia Fantasma', 81, '7/9/2022', true, false, false, false),
+	('7277', 'Rose Dragon', 'Conjuro de Rosas', 78, '8/9/2022', true, false, false, false),
+	('1513', 'Elemental Heros', 'Duelo Favorito', 108, '8/9/2022', true, false, false, false),
+	('3494', 'Phantom Knights', 'La Orden de los Caballeros Fantasma', 83, '8/9/2022', true, false, false, false),
+	('8246', 'Harpies', 'Última Voluntad de Arpías', 46, '8/9/2022', true, false, false, false),
+	('2328', 'Odd-Eyes', 'Xiangke y Xiangsheng', 93, '8/9/2022', true, false, false, false),
+	('1981', 'Rose Dragon', 'Conjuro de Rosas', 45, '8/9/2022', true, false, false, false),
+	('9861', 'Necroz', 'Maestro de Rituales', 65, '8/9/2022', true, false, false, false),
+	('2671', 'Phantom Knights', 'La Orden de los Caballeros Fantasma', 39, '8/9/2022', true, false, false, false),
+	('9540', 'Rose Dragon', 'Conjuro de Rosas', 115, '8/9/2022', true, false, false, false),
+	('5294', 'Destiny Heros', 'Reloj del Destino', 61, '8/9/2022', true, false, false, false),
+	('2185', 'Odd-Eyes', 'Xiangke y Xiangsheng', 65, '8/9/2022', true, false, false, false),
+	('2848', 'Lunalight', 'Robo del Destino', 92, '8/9/2022', true, false, false, false);
+--	
+insert into kcsep22 (usuario, deck, skill, wins, ndmax, zerotg, zephra, bryan, xenoblur) 
+values 
+	('5407', 'Infernity', 'Fantasma Hueco', 124, '7/9/2022', false, true, false, false),
+	('1225', 'War Rock', 'Oleada de Iluminación', 135, '7/9/2022', false, true, false, false),
+	('5020', 'Abyss Actor', 'Tumbas Selladas', 72, '8/9/2022', false, true, false, false),
+	('7553', 'Melodious', 'de Cantante a Maestra', 58, '8/9/2022', false, true, false, false),
+	('4409', 'Meklord', 'Refinamiento Meklord', 77, '8/9/2022', false, true, false, false),
+	('6412', 'Necroz', 'Maestro de Rituales', 94, '8/9/2022', false, true, false, false),
+	('8171', 'Heros', 'Maestro de Fusión', 161, '8/9/2022', false, true, false, false);
+--	
+insert into kcsep22 (usuario, deck, skill, wins, ndmax, zerotg, zephra, bryan, xenoblur) 
+values 
+	('3256', 'D/D/D', 'El Mando del Rey de la Perdición', 127, '6/9/2022', false, false, true, false),
+	('8875', 'Fluffal', 'El Monstruo más imparable', 44, '6/9/2022', false, false, true, false),
+	('9548', 'D/D/D', 'El Mando del Rey de la Perdición', 94, '7/9/2022', false, false, true, false),
+	('6346', 'Buster Blader', 'Robar sentido Nivel bajo', 134, '8/9/2022', false, false, true, false),
+	('0803', 'D/D/D', 'El Mando del Rey de la Perdición', 134, '8/9/2022', false, false, true, false);
+--
+-- Datos sin wins
+insert into kcsep22 (usuario, deck, skill, ndmax, zerotg, zephra, bryan, xenoblur) 
+values 
+	('1168', 'Elemental Heros', 'Duelo Favorito', '6/9/2022', true, false, false, false),
+	('0789', 'Tech Genus', 'Listos Aceleración Delta', '7/9/2022', true, false, false, false),
+	('9881', 'Rose Dragon', 'Conjuro de Rosas', '7/9/2022', true, false, false, false),
+	('8510', 'Abyss Actor', 'Tumbas Selladas', '8/9/2022', true, false, false, false),
+	('1463', 'Elemental Heros', 'Duelo Favorito', '8/9/2022', true, false, false, false);
+--	
+insert into kcsep22 (usuario, deck, skill, ndmax, zerotg, zephra, bryan, xenoblur) 
+values 
+	('4680', 'Gandora', 'Luchar con Decisión', '8/9/2022', false, true, false, false);
+--
+insert into kcsep22 (usuario, deck, skill, ndmax, zerotg, zephra, bryan, xenoblur) 
+values 
+	('7116', 'Rose Dragon', 'Conjuro de Rosas', '8/9/2022', false, false, true, false),
+	('5226', 'Ancient Gears', 'A Luchar Mecanismo Antiguo', '8/9/2022', false, false, true, false);
+--	
+-- Búsquedas de decks y skills
+select deck, skill from july22 where deck like '%x%';
+select deck, skill from ago22 where deck like '%ech%';
+select deck, skill from ago22 where deck like '%ha%';
+select deck, skill from ago22 where deck like '%%';
+select deck, skill from ago22 where deck like '%dd%';
+select deck, skill from ago22 where deck like '%una%';
+select deck, skill from ago22 where deck like '%uster%';
+select deck, skill from ago22 where deck like '%nci%';
+select deck, skill from kcsep22 where deck like '%x%';
+-- Actualizar datos
+select * from kcsep22 where usuario like '%9310%';
+update kcsep22 set zephra = true where id = 3;
+select * from kcsep22 where usuario like '%3993%';
+select * from kcsep22 where usuario like '%9651%';
+update kcsep22 set zephra = true where id = 6;
+select * from kcsep22 where usuario like '%5407%';
+select * from kcsep22 where usuario like '%9361%';
+update kcsep22 set zephra = true where id = 10;
+select * from kcsep22 where usuario like '%9881%';
+update kcsep22 set zephra = true where id = 30;
+select * from kcsep22 where usuario like '%1225%';
+select * from kcsep22 where usuario like '%1892%';
+update kcsep22 set zephra = true where id = 15;
+select * from kcsep22 where usuario like '%8510%';
+update kcsep22 set zephra = true where id = 31;
+select * from kcsep22 where usuario like '%5020%';
+update kcsep22 set zephra = true where id = 31;
+select * from kcsep22 where usuario like '%4680%';
+select * from kcsep22 where usuario like '%7553%';
+select * from kcsep22 where usuario like '%4409%';
+select * from kcsep22 where usuario like '%9540%';
+update kcsep22 set zephra = true where id = 24;
+select * from kcsep22 where usuario like '%2185%';
+update kcsep22 set zephra = true where id = 26;
+--
+select * from kcsep22 where usuario like '%9310%';
+update kcsep22 set bryan = true where id = 3;
+select * from kcsep22 where usuario like '%9651%';
+update kcsep22 set bryan = true where id = 6;
+select * from kcsep22 where usuario like '%3256%';
+select * from kcsep22 where usuario like '%8875%';
+select * from kcsep22 where usuario like '%3867%';
+update kcsep22 set bryan = true where id = 7;
+select * from kcsep22 where usuario like '%3494%';
+update kcsep22 set bryan = true where id = 18;
+select * from kcsep22 where usuario like '%9548%';
+select * from kcsep22 where usuario like '%8510%';
+update kcsep22 set bryan = true where id = 31;
+select * from kcsep22 where usuario like '%6346%';
+select * from kcsep22 where usuario like '%9540%';
+update kcsep22 set bryan = true where id = 24;
+select * from kcsep22 where usuario like '%5294%';
+update kcsep22 set bryan = true where id = 25;
+select * from kcsep22 where usuario like '%7116%';
+update kcsep22 set bryan = true where id = 25;
+select * from kcsep22 where usuario like '%5226%';
+--
+select * from kcsep22 where usuario like '%9651%';
+update kcsep22 set xenoblur = true where id = 6;
+select * from kcsep22 where usuario like '%9861%';
+update kcsep22 set xenoblur = true where id = 22;
+select * from kcsep22 where usuario like '%9540%';
+update kcsep22 set xenoblur = true where id = 24;
+select * from kcsep22 where usuario like '%1513%';
+update kcsep22 set xenoblur = true where id = 17;
+select * from kcsep22 where usuario like '%1513%';
+update kcsep22 set xenoblur = true where id = 17;
+--
---
+++ report-days-sql/0-2-dia9.sql
@@ -0,0 +1,110 @@
+-- 9/09/22
+insert into kcsep22 (usuario, deck, skill, wins, ndmax, zerotg, zephra, bryan, xenoblur) 
+values 
+	('6913', 'Synchrons', 'Camino de la Estrella Fugaz', 54, '8/9/2022', true, false, false, false),
+	('8799', 'Rose Dragon', 'Conjuro de Rosas', 132, '8/9/2022', true, false, false, false),
+	('8075', 'Fluffal', 'Precognición', 132, '9/9/2022', true, false, false, false),
+	('3621', 'Phantom Knights', 'La Orden de los Caballeros Fantasma', 80, '9/9/2022', true, false, false, false),
+	('1725', 'Satellarknights', 'Robo del Destino', 31, '9/9/2022', true, false, false, false),
+	('2127', 'Altergeist', 'Reacción en Cadena', 111, '9/9/2022', true, false, false, false),
+	('6010', 'Evil Heros', 'El poder conduce al Destino', 95, '9/9/2022', true, false, false, false),
+	('0487', 'Rose Dragon', 'Conjuro de Rosas', 81, '9/9/2022', true, false, false, false),
+	('0955', 'Dark Magician', 'Robo del Destino', 140, '9/9/2022', true, false, false, false),
+	('2945', 'Rose Dragon', 'Conjuro de Rosas', 55, '9/9/2022', true, false, false, false),
+	('5468', 'Elemental Heros', 'Duelo Favorito', 181, '9/9/2022', true, false, false, false),
+	('8906', 'Abyss Actor', 'Tumbas Selladas', 89, '9/9/2022', true, false, false, false),
+	('9017', 'Malefic', 'Territorio de Malicia', 123, '9/9/2022', true, false, false, false),
+	('3940', 'Phantom Knights', 'Estrategia Fantasma', 133, '9/9/2022', true, false, false, false),
+	('2250', 'Mayosenju', 'Neo Nuevo Sylvio', 83, '9/9/2022', true, false, false, false);
+--	
+insert into kcsep22 (usuario, deck, skill, wins, ndmax, zerotg, zephra, bryan, xenoblur) 
+values 
+	('8504', 'Red-Eyes', 'Ruleta de Ojos Rojos', 140, '9/9/2022', false, true, false, false),
+	('3248', 'Synchrons', 'Camino de la Estrella Fugaz', 192, '9/9/2022', false, true, false, false),
+	('6325', 'Evil Eye', 'Afinación de Nivel', 217, '9/9/2022', false, true, false, false),
+	('7821', 'Elemental Heros', 'Duelo Favorito', 70, '9/9/2022', false, true, false, false),
+	('7322', 'Elemental Heros', 'Duelo Favorito', 92, '9/9/2022', false, true, false, false);
+--	
+insert into kcsep22 (usuario, deck, skill, wins, ndmax, zerotg, zephra, bryan, xenoblur) 
+values 
+	('8294', 'Galaxy-Eyes', 'Galaxia XYZ', 111, '9/9/2022', false, false, true, false),
+	('4540', 'War Rock', 'Oleada de Iluminación', 61, '9/9/2022', false, false, true, false),
+	('9657', 'Metalfoes', 'Barrera de Luz', 105, '9/9/2022', false, false, true, false),
+	('8829', 'Six Samurais', 'Vinculaciones', 97, '9/9/2022', false, false, true, false),
+	('3734', 'War Rock', 'Oleada de Iluminación', 98, '9/9/2022', false, false, true, false),
+	('9424', 'Satellarknights', 'Robo del Destino', 82, '9/9/2022', false, false, true, false);
+--
+insert into kcsep22 (usuario, deck, skill, wins, ndmax, zerotg, zephra, bryan, xenoblur) 
+values 
+	('1705', 'Malefic', 'Territorio de Malicia', 63, '9/9/2022', false, false, false, true);
+--		
+-- Datos sin wins
+insert into kcsep22 (usuario, deck, skill, ndmax, zerotg, zephra, bryan, xenoblur) 
+values 
+	('1137', 'Fossil', 'Guerreros Fósiles', '9/9/2022', true, false, false, false),
+	('4058', 'Rose Dragon', 'Conjuro de Rosas', '9/9/2022', true, false, false, false),
+	('4266', 'Six Samurais', 'Robar sentido Viento', '9/9/2022', true, false, false, false);
+--
+insert into kcsep22 (usuario, deck, skill, ndmax, zerotg, zephra, bryan, xenoblur) 
+values 
+	('6036', 'Qliphort', 'Carga de ataque', '9/9/2022', false, false, false, true);	
+--
+-- Búsquedas de decks y skills
+select deck, skill from july22 where deck like '%ync%';
+select deck, skill from ago22 where deck like '%atel%';
+select deck, skill from ago22 where deck like '%vil%';
+select deck, skill from ago22 where deck like '%en%';
+select deck, skill from july22 where deck like '%li%';
+select deck, skill from kcsep22 where deck like '%ha%';
+select deck, skill from kcsep22 where deck like '%ale%';
+
+-- Actualizar datos
+select * from kcsep22 where usuario like '%0803%';
+update kcsep22 set zerotg = true where id = 39;
+--
+select * from kcsep22 where usuario like '%8504%';
+select * from kcsep22 where usuario like '%3621%';
+update kcsep22 set zephra = true where id = 54;
+select * from kcsep22 where usuario like '%3248%';
+select * from kcsep22 where usuario like '%1137%';
+update kcsep22 set zephra = true where id = 48;
+select * from kcsep22 where usuario like '%2127%';
+update kcsep22 set zephra = true where id = 56;
+select * from kcsep22 where usuario like '%6325%';
+select * from kcsep22 where usuario like '%0487%';
+update kcsep22 set zephra = true where id = 58;
+select * from kcsep22 where usuario like '%5468%';
+update kcsep22 set zephra = true where id = 61;
+select * from kcsep22 where usuario like '%7821%';
+select * from kcsep22 where usuario like '%3940%';
+update kcsep22 set zephra = true where id = 64;
+--
+select * from kcsep22 where usuario like '%8294%';
+select * from kcsep22 where usuario like '%3621%';
+update kcsep22 set bryan = true where id = 54;
+select * from kcsep22 where usuario like '%1725%';
+update kcsep22 set bryan = true where id = 55;
+select * from kcsep22 where usuario like '%4540%';
+select * from kcsep22 where usuario like '%8829%';
+select * from kcsep22 where usuario like '%0487%';
+update kcsep22 set bryan = true where id = 58;
+select * from kcsep22 where usuario like '%2250%';
+update kcsep22 set bryan = true where id = 65;
+select * from kcsep22 where usuario like '%3734%';
+select * from kcsep22 where usuario like '%9424%';
+---
+select * from kcsep22 where usuario like '%3621%';
+update kcsep22 set xenoblur = true where id = 54;
+select * from kcsep22 where usuario like '%8246%';
+update kcsep22 set xenoblur = true where id = 19;
+select * from kcsep22 where usuario like '%5498%';
+update kcsep22 set xenoblur = true where id = 11;
+select * from kcsep22 where usuario like '%5468%';
+update kcsep22 set xenoblur = true where id = 61;
+select * from kcsep22 where usuario like '%3734%';
+update kcsep22 set xenoblur = true where id = 75;
+select * from kcsep22 where usuario like '%7322%';
+update kcsep22 set xenoblur = true where id = 70;
+select * from kcsep22 where usuario like '%1705%';
+update kcsep22 set xenoblur = true where id = 70;
+--
